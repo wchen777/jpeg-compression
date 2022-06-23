a7a3c2321f43137cb7f0928c7d0ecc1a69dd86c6
Will Chen <william_b_chen@brown.edu>
2022-06-22 23:23:35 -0400

    add comments

--- include/utility.hpp
+++ include/utility.hpp
@@ -17,35 +17,37 @@ namespace jpeg_compression
 
     inline const bool isValidChar(const char ch)
     {
-        return isprint(ch) || ch != '/' || ch != '\\';
+        return isprint(ch) || ch != '/' || ch != '\\'; // valid characters in filepaths
     }
 
-    inline const bool isValidFilename(const std::string& filename)
+    inline const bool isValidFilename(const std::string &filename)
     {
         for (auto&& c : filename)
-            if (!isValidChar(c))
+            if (!isValidChar(c)) // check for valid characters in filepath
                 return false;
 
-        std::size_t extPos = filename.find(".jpg");
+        std::size_t extPos = filename.find(".jpg"); // look for jpg extension
 
         if (extPos == std::string::npos)
-            extPos = filename.find(".jpeg");
+            extPos = filename.find(".jpeg"); // if not found, look for jpeg extension
 
         else if (extPos + 4 == filename.size())
-            return true;
+            return true; // if either are found return true
 
         if (extPos == std::string::npos)
-            return false;
+            return false; // otherwise the filepath is not valid
 
         return false;
     }
 
+    // check if a character is any form of whitespace (used in below function)
     inline const bool isWhiteSpace(const char ch)
     {
         return iscntrl(ch) || isblank(ch) || isspace(ch);
     }
 
-    inline const bool isStringWhiteSpace(const std::string& str)
+    // check if a given string is entirely whitespace
+    inline const bool isStringWhiteSpace(const std::string &str)
     {
         for (auto&& c : str)
             if (!isWhiteSpace(c))
